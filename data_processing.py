b874612b1f558697813eec5429e6685d69d40f7e
aerokhaleel <aerokhaleel@gmail.com>
2022-04-30 14:05:29 +0530

    correcte code

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,20 @@
 # Khaleel
+from regex import D
+
+
 print("adding all the data processing code related fraud detection")
+
+
+a = 5
+b = 7 
+
+def sum(a,b):
+    c = a + b
+    return c
+
+print ("sum function")
+
+def multiplication(a,b):
+    d = a*b
+    return d
+print ("Multiplication function")
