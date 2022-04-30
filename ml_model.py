a79c5a033e38dfa72aa5f5b1086469d60f6aacec
aerokhaleel <aerokhaleel@gmail.com>
2022-04-30 14:36:00 +0530

    Added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("ML")
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print("adding code for ML model1")
+
+def model3():
+    print("adding code for ML model1")
