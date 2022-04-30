e8f4f373bed52f2e214977f87c0a5fffbc04174d
aerokhaleel <aerokhaleel@gmail.com>
2022-04-30 13:57:18 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,2 @@
 # Khaleel
 print("adding all the data processing code related fraud detection")
-
-
-a = 5
-b = 7
-
-def sum(a,b):
-    c = a + b
-    return c
-
-print ("added a new function sum")
